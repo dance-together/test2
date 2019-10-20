1844881acad8be53eaa0fe3d9e8d822bd522e3c8
Sami <sami-weso@hotmail.com>
2019-10-20 12:03:40 +0200

    master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { TreatmentComponent } from './treatment/treatment.component';
+import { DevelopmentComponent } from './development/development.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent},
+  { path:'home', component: HomeComponent},
+  { path:'contact', component: ContactComponent},
+  { path:'development', component: DevelopmentComponent},
+  { path:'treatment', component: TreatmentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { TreatmentComponent } from './treatment/treatment.component';
+import { DevelopmentComponent } from './development/development.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ContactComponent,
+    TreatmentComponent,
+    DevelopmentComponent
   ],
   imports: [
     BrowserModule,
