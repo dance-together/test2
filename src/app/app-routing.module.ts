7af4b895537ee1d6097fe9dd09dda39811308597
Sami <sami-weso@hotmail.com>
2019-11-23 18:30:31 +0100

    footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 const routes: Routes = [
-  {path:'header',component:HeaderComponent}
+  {path:'header',component:HeaderComponent},
+  {path:'header',component:FooterComponent}
 ];
 
 @NgModule({
