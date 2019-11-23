3eceba2fa5517f248c785a70fb957b0cc75f4562
Sami <sami-weso@hotmail.com>
2019-11-23 19:16:31 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { AlternativMedicinComponent } from './alternativ-medicin/alternativ-medicin.component';
-
+import { AlternativMedicinComponent} from './alternativ-medicin/alternativ-medicin.component'
 
 const routes: Routes = [
   { path:'', component: HomeComponent},
   {path:'header',component:HeaderComponent},
   {path:'header',component:FooterComponent},
   {path:'home',component:HomeComponent},
-  {path:'alternativ-medicin',component:AlternativMedicinComponent}
+  {path:'alternativ-medicin', component:AlternativMedicinComponent}
 ];
 
 @NgModule({
