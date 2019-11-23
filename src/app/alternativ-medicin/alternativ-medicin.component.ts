390ce4ad21da72300e04b91402251474000a3e95
Sami <sami-weso@hotmail.com>
2019-11-23 18:49:23 +0100

    alternativ-medicin

---
+++ src/app/alternativ-medicin/alternativ-medicin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alternativ-medicin',
+  templateUrl: './alternativ-medicin.component.html',
+  styleUrls: ['./alternativ-medicin.component.css']
+})
+export class AlternativMedicinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
