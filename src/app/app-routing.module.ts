c4cc3ac40eea6cefb328053451b4a47f26531e22
Sami <sami-weso@hotmail.com>
2019-11-23 19:29:56 +0100

    development

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { DevelopmentComponent } from './development/development.component';
 //import { AlternativMedicinComponent } from './alternativ-medicin/alternativ-medicin.component';
 
 
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path:'header',component:FooterComponent},
   {path:'home',component:HomeComponent},
  // {path:'alternativ-medicin',component:AlternativMedicinComponent}
- {path:'contact', component:ContactComponent}
+ {path:'contact', component:ContactComponent},
+ {path:'development',component:DevelopmentComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { DevelopmentComponent } from './development/development.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContactComponent } from './contact/contact.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    DevelopmentComponent
   ],
   imports: [
     BrowserModule,
