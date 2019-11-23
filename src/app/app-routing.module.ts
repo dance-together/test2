ebba78a2a6b9f47de9e179195eed6c8cd6442d19
Sami <sami-weso@hotmail.com>
2019-11-23 18:42:37 +0100

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
+  { path:'', component: HomeComponent},
   {path:'header',component:HeaderComponent},
-  {path:'header',component:FooterComponent}
+  {path:'header',component:FooterComponent},
+  {path:'home',component:HomeComponent}
 ];
 
 @NgModule({
